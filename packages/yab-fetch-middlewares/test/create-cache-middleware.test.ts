b640c4bee0ac625359779ee87dac15a1dfdb605d
william.guo <809908337@qq.com>
2019-07-23 23:00:04 +0800

    test: add test case

--- packages/yab-fetch-middlewares/test/create-cache-middleware.test.ts
+++ packages/yab-fetch-middlewares/test/create-cache-middleware.test.ts
@@ -1,5 +1,8 @@
 import 'fake-indexeddb/auto';
+import 'whatwg-fetch';
+import { IYabFetchContext } from 'yab-fetch';
 import createIDBStore, { IDBCacheStorage } from '../src/cache/idb';
+import { createCacheMiddleware } from '../src/index';
 import { promisifyRequest } from '../src/cache/utils';
 
 describe('IDB Store', () => {
@@ -42,5 +45,97 @@ describe('IDB Store', () => {
     const result = await idbStore.get(url);
 
     expect(result).toMatchObject({ url, data });
+  });
+});
+
+describe('Cache Middleware', () => {
+  const url = '/github';
+  const data = { id: 1, username: 'yabfetch' };
+  let idbStore: IDBCacheStorage;
+
+  beforeAll(async () => {
+    idbStore = createIDBStore();
+  });
+
+  beforeEach(async () => {
+    await idbStore.init();
+    await idbStore.set(url, data);
+  });
+
+  afterAll(async () => {
+    idbStore.request.result.close();
+  });
+
+  test('strategy: always, get data from cache directly', async () => {
+    const cacheMiddleware = createCacheMiddleware({ strategy: 'always' });
+    const fakeNext = jest.fn();
+    const fakeCtx: IYabFetchContext = {
+      yabRequestInit: { url, contentType: 'json' },
+      response: new Response(),
+      error: undefined
+    };
+
+    await cacheMiddleware(fakeCtx, fakeNext);
+
+    expect(fakeCtx.json).toMatchObject(data);
+    expect(fakeNext.mock.calls.length).toBe(0);
+  });
+
+  test('strategy: fallback, set cache after fetch', async () => {
+    const cacheMiddleware = createCacheMiddleware({ strategy: 'fallback' });
+    const fakeNext = jest.fn();
+    const fakeCtx: IYabFetchContext = {
+      yabRequestInit: { url, contentType: 'json' },
+      response: new Response('{"id":"3"}'),
+      json: { id: 3 },
+      error: undefined
+    };
+
+    await cacheMiddleware(fakeCtx, fakeNext);
+
+    await idbStore.init();
+    const cache = await idbStore.get(url);
+    expect(cache.data).toMatchObject({ id: 3 });
+    expect(fakeNext.mock.calls.length).toBe(1);
+  });
+
+  test('strategy: fallback, get data from cache if fetch failed', async () => {
+    const cacheMiddleware = createCacheMiddleware({ strategy: 'fallback' });
+    const fakeNext = jest.fn(() => {
+      throw new Error('fetch error');
+    });
+    const fakeCtx: IYabFetchContext = {
+      yabRequestInit: { url, contentType: 'json' },
+      response: new Response(),
+      error: undefined
+    };
+
+    await cacheMiddleware(fakeCtx, fakeNext);
+
+    expect(fakeCtx.json).toMatchObject(data);
+    expect(fakeNext.mock.calls.length).toBe(1);
+  });
+
+  test('strategy: fallback, throw error if fetch failed and no cache data exist', async () => {
+    const cacheMiddleware = createCacheMiddleware({ strategy: 'fallback' });
+    const fakeError = new Error('fetch error');
+    const fakeNext = jest.fn(() => {
+      throw fakeError;
+    });
+    const fakeCtx: IYabFetchContext = {
+      yabRequestInit: { url: '/react', contentType: 'json' },
+      response: new Response(),
+      error: undefined
+    };
+
+    let error;
+    try {
+      await cacheMiddleware(fakeCtx, fakeNext);
+    } catch (e) {
+      error = e;
+    }
+
+    expect(error).toBe(fakeError);
+    expect(fakeNext.mock.calls.length).toBe(1);
   });
 });
