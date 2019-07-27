6980e8e225afc32a66356473267bd6fe1fa9f74f
Leo Hui <leohxj@gmail.com>
2019-07-27 15:40:10 +0800

    feat: make console color

--- packages/yab-fetch-middleware-log/src/index.ts
+++ packages/yab-fetch-middleware-log/src/index.ts
@@ -3,11 +3,41 @@ import { IYabFetchContext, YabFetchMiddleware } from 'yab-fetch';
 import { Options } from './type';
 
 export const createLogger = (options: Options): YabFetchMiddleware => {
-  const { color } = options;
+  const { collapsed = true } = options;
+
+  const { log, error, groupCollapsed, group, groupEnd } = console;
+
+  const logger = {
+    log,
+    group: collapsed ? groupCollapsed : group,
+    groupEnd,
+    error,
+  };
 
   return async (ctx: IYabFetchContext, next: () => Promise<unknown>) => {
-    console.log('ctx before:', ctx);
-    await next();
-    console.log('ctx after:', ctx);
+    const {
+      yabRequestInit,
+      yabRequestInit: { url, method },
+      response,
+    } = ctx;
+
+    logger.group(
+      `📖 yab: %c${method} %c${url} %c @${new Date()}`,
+      'color: #6f42c1;font-size: 14px;',
+      'color: #005cc5;font-size: 14px;',
+      'color: #666'
+    );
+    logger.group('before fetch', 'color: #33b9f9');
+    logger.log('yabRequestInit:', yabRequestInit);
+    logger.groupEnd();
+    try {
+      await next();
+      logger.group('after fetch', 'color:#61bb64');
+      logger.log('response:', response);
+      logger.groupEnd();
+    } catch (e) {
+      logger.groupEnd();
+      logger.error(`Something wrong with fetch: ${e}`);
+    }
   };
 };
--- packages/yab-fetch-middleware-log/src/type.ts
+++ packages/yab-fetch-middleware-log/src/type.ts
@@ -1,3 +1,3 @@
 export interface Options {
-  color: boolean;
+  collapsed: boolean;
 }
